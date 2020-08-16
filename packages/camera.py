76255540243ce0bea908980fff30a41ebb64b798
Mr.Mario <38796774+YYYYifan@users.noreply.github.com>
2020-08-16 14:17:21 +0100

    Create camera.py

---
+++ packages/camera.py
@@ -0,0 +1,48 @@
+import cv2
+import datetime
+
+def gstreamer_pipeline(
+        capture_width=1280,
+        capture_height=720,
+        display_width=1280,
+        display_height=720,
+        framerate=60,
+        flip_method=0,
+        ):
+        return (
+            "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+            )
+        )
+
+class camera:
+    def __init__(self, grayscale: bool=False):        
+        self.grayscale = grayscale
+
+        self.cam = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+
+        if self.cam.isOpened():
+            print("Camera is ready")
+
+
+    def take_a_pic(self):
+        _, image = self.cam.read()
+        if self.grayscale:
+            image = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+        return image
+
+    
+    def save_pic(self, image):
+        cv2.imwrite('./images/camera/{}.png'.format(datetime.datetime.now()), image)
+        print("Finish")
+        
