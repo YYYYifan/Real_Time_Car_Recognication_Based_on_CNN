b3784dd0d5f5cbd6de9025e2c1dda402d3299066
Mr.Mario <duyifan0326@gmail.com>
2020-08-22 19:44:44 +0100

    code

--- Loss_Acc_Visualization.py
+++ Loss_Acc_Visualization.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker 
+import matplotlib.ticker as ticker
 import json
 
 
@@ -10,7 +9,7 @@ with open("./parameter.json", 'r') as file_obj:
     parameter = json.load(file_obj)
     index_size = parameter["index_size"]
     batch_size = parameter["batch_size"]
-    
+
 with open("./result/train_torch_1.4.0.log") as file_obj:
     buffer = file_obj.readlines()
 
@@ -20,20 +19,22 @@ for index in range(len(buffer)):
     buffer[index] = buffer[index].replace("[", "")
     buffer[index] = buffer[index].replace("]", "")
     buffer[index] = buffer[index].split(" ")
-    
-result_log = np.asarray(buffer).astype(float)    
+
+result_log = np.asarray(buffer).astype(float)
 
 loss = []
 for index in range(len(result_log)):
-    if index % index_size == 0:        
-        loss.append(np.sum(result_log[index: index + index_size, 2]) / index_size)
+    if index % index_size == 0:
+        loss.append(
+            np.sum(result_log[index: index + index_size, 2]) / index_size)
 
 loss = np.asarray(loss)
 
 accuarcy = []
 for index in range(len(result_log)):
-    if index % index_size == 0:        
-        accuarcy.append(np.sum(result_log[index: index + index_size, 3]) / index_size)
+    if index % index_size == 0:
+        accuarcy.append(
+            np.sum(result_log[index: index + index_size, 3]) / index_size)
 
 accuarcy = np.asarray(accuarcy)
 
@@ -47,9 +48,11 @@ plt.xlabel("Epoch")
 
 
 def to_percent(temp, position):
-    return '%1.0f'%(100*temp) + '%'
+    return '%1.0f' % (100*temp) + '%'
+
 
 plt.gca().yaxis.set_major_formatter(ticker.FuncFormatter(to_percent))
 
-plt.savefig("./images/Loss_and_Accuracy_in_Training.png", dpi = 900,  bbox_inches='tight')
+plt.savefig("./images/Loss_and_Accuracy_in_Training.png",
+            dpi=900,  bbox_inches='tight')
 plt.show()
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ batch_size = 12
 
 with open("./parameter.json", 'r') as file_obj:
     parameter = json.load(file_obj)
-    len_each_subset_in_train = parameter["len_each_subset_in_train"]    
+    len_each_subset_in_train = parameter["len_each_subset_in_train"]
 
 
 myImage = prepare.imagePocess(save=True)
@@ -23,10 +23,11 @@ print("Device: {}".format(device))
 # Load datas to torchvision.dataset .
 train_dataset = dataset.Mydataset(myImage.train, len_each_subset_in_train)
 # Load dataset to torchvision.dataloder .
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+train_dataloader = DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True)
 
 # Load Convorlotion Neural Network Models.
-net = models.MobileNetV2(2)    
+net = models.MobileNetV2(2)
 net.to(device)
 
 # Define optimizer and loss func.
@@ -43,43 +44,44 @@ def adjust_learning_rate(optimizer, epoch, lr):
 
 print("Start Train")
 with open("./result/train_torch_{}.log".format(torch.__version__), "w") as log_obj:
-    start = datetime.datetime.now()    
+    start = datetime.datetime.now()
     index_size = 0
     for time in range(epoch_size):
-        adjust_learning_rate(optimizer,time,learnRate)
+        adjust_learning_rate(optimizer, time, learnRate)
         total = 0
         correct = 0
         net.train(True)
-        for index, (image,labels) in enumerate(train_dataloader):
-                image = image.to(device)
-                labels = labels.to(device)
-                optimizer.zero_grad()
-                # print("input shape",image.shape)
-                outputs = net(image)
-                # print(outputs.shape,labels.shape)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()                
-                _, predicted = outputs.max(1)
-                total += outputs.size(0)
-                correct += predicted.eq(labels).sum().item()
-                Accuracy = correct/total
-                print("Epoch: {}/{}, idx:{} Loss:{:.4f} Accuracy:{:.4f}".format(time+1,epoch_size, index, loss,Accuracy))
-                logData = [time, index, loss.item(), Accuracy]
-                log_obj.write(str(logData))
-                log_obj.write("\n")
-                
+        for index, (image, labels) in enumerate(train_dataloader):
+            image = image.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            # print("input shape",image.shape)
+            outputs = net(image)
+            # print(outputs.shape,labels.shape)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            _, predicted = outputs.max(1)
+            total += outputs.size(0)
+            correct += predicted.eq(labels).sum().item()
+            Accuracy = correct/total
+            print("Epoch: {}/{}, idx:{} Loss:{:.4f} Accuracy:{:.4f}".format(time +
+                                                                            1, epoch_size, index, loss, Accuracy))
+            logData = [time, index, loss.item(), Accuracy]
+            log_obj.write(str(logData))
+            log_obj.write("\n")
+
         parameter["index_size"] = index+1
         parameter["batch_size"] = time
 
 # Release video memory (if it`s available).
 torch.cuda.empty_cache()
-end = datetime.datetime.now()      
+end = datetime.datetime.now()
 print("Train Time: {}".format(end-start))
 
 # save model
 torch.save(net, './result/net_torch_{}.pkl'.format(torch.__version__))
 # save parameter.json
 with open('./parameter.json', 'w') as file_obj:
-    json.dump(parameter, file_obj, sort_keys=True, indent=4, separators=(',', ':'))
-
+    json.dump(parameter, file_obj, sort_keys=True,
+              indent=4, separators=(',', ':'))
